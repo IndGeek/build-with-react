3c702ae33b97dae9605bd8b7163617543bda08e8
lifeofsoumya <mondalsoumya2001@gmail.com>
2023-04-26 23:25:10 +0530

    model added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import Model from "./components/Model";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Model popTitle="Register">
+        <h1>Hi, apply here</h1>
+        <h4>Start filling the form!</h4>
+        <form className="flex flex-col">
+          <input type="text" placeholder="Enter name" />
+          <input type="password" placeholder="Set a Password" />
+          <input type="submit" value="Sign up" className="btn" />
+        </form>
+      </Model>
+      <Model popTitle="Log In">
+        <h1>Welcome back</h1>
+        <h4>Fill Details</h4>
+        <form className="flex flex-col">
+          <input type="text" placeholder="Enter Username" />
+          <input type="password" placeholder="Password" />
+          <input type="submit" value="Log In" className="btn" />
+        </form>
+      </Model>
     </div>
   );
 }
-
-export default App;
