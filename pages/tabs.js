26f2a9275ad82d085d565da1993475b8b5d4a0e0
soumya <mondalsoumya2001@gmail.com>
2023-11-28 12:54:41 +0530

    tabs modified

--- pages/tabs.js
+++ pages/tabs.js
@@ -2,14 +2,14 @@ import TabComponent from "@/components/tabs/Tabcomponent";
 
 const Tabs = () => {
     const tabData = [
-      {id: 1, title: 'tab1', content: 'ok'},
-      {id: 2, title: 'tab2', content: 'lol'},
+      {id: 1, title: 'Code', content: 'Code' },
+      {id: 2, title: 'Info', content: 'lol'},
+      {id: 3, title: 'Source', content: 'd'},
     ]
     return (
-      <>
-      <div>User</div>
+      <div className="h-screen w-full flex items-start justify-center pt-24">
       <TabComponent tabData={tabData}/>
-      </>
+      </div>
     )
   }
 
