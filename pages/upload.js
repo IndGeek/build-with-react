cf1703be3dfc00f63bf087bb3a454418cbd9a8e0
lifeofsoumya <mondalsoumya2001@gmail.com>
2024-03-22 13:56:47 +0530

    upload finalization done

--- pages/upload.js
+++ pages/upload.js
@@ -10,6 +10,7 @@ const Upload = () => {
   const [progress, setProgress] = useState(0);
   const fileRef = useRef();
   const [status, setStatus] = useState('idle');
+  const [res, setRes] = useState(null)
   const allowedSize = 50 * 1024;
   const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_NAME
 
@@ -19,14 +20,15 @@ const Upload = () => {
 
     console.log(e.target.files[0], " file ---");
 
-    if (e.target.files[0].size > allowedSize){
-      fileRef.current.value = '';
-      return toast.error("Please choose file less than 50kb");
-    }
+    // if (e.target.files[0].size > allowedSize){
+    //   fileRef.current.value = '';
+    //   return toast.error("Please choose file less than 50kb");
+    // }
 
     try {
       setStatus("uploading");
       const file = e.target.files[0];
+      setFile(file)
       const formData = new FormData();
       formData.append("file", file);
       formData.append("upload_preset", process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET);
@@ -46,6 +48,8 @@ const Upload = () => {
         })
         .then((res) => {
           setStatus("done"); // set status to 'done'
+          setRes(res?.data)
+          console.log({res})
           toast.success("File uploaded successfully");
         })
         .catch((error) => {
@@ -65,9 +69,9 @@ const Upload = () => {
 
   return (
     <div className="w-[250px] m-auto flex flex-col items-center justify-center h-screen">
-      {status !== 'idle' &&
+      {status !== 'idle' && status !== 'done' && 
       <span className="progress bg-gray-200 rounded w-full h-3 mb-4 relative">
-        <p className="absolute -translate-y-4 right-0 top-0 text-xs">{progress}%</p>
+        <p className="absolute -translate-y-4 right-0 top-0 text-xs text-indigo-800">{progress}%</p>
         <span className="filled_state h-3 rounded bg-indigo-500 z-1 absolute top-0 left-0" style={{width: `${progress}%`}}></span>
       </span>
       }
@@ -77,13 +81,13 @@ const Upload = () => {
         <label htmlFor="file_up_btn">
             <div className="border-2 w-[250px] border-dotted border-indigo-500 p-4 rounded-lg flex flex-col gap-2 items-center cursor-pointer">
                 <DocumentArrowUpIcon className='w-10 h-10 text-indigo-600' />
-                {file?.name.substring(0, 16)}
+                {file?.name.substring(0, 16)}{file?.name.length>16 && `.${file?.name.split('.')[1]}`}
                 <p className="text-indigo-500">Upload a File</p>
             </div>
         </label>
       </>
       }
-      {status == 'done' && <p className="text-indigo-500 text-sm">Uploaded successfully</p>}
+      {status == 'done' && <p className="text-indigo-900 text-xs">You can access the file at {res.secure_url}</p>}
     </div>
   );
 };
